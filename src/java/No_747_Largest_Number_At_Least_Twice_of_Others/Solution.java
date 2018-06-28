9457549c9763b956be6f7911dd168a85a0d67cd1
Charles <dailiang18bb@gmail.com>
2018-06-27 20:32:46 -0400

    747

--- src/java/No_747_Largest_Number_At_Least_Twice_of_Others/Solution.java
+++ src/java/No_747_Largest_Number_At_Least_Twice_of_Others/Solution.java
@@ -1,5 +1,52 @@
 package java.No_747_Largest_Number_At_Least_Twice_of_Others;
 
-public class Solution {
-
+class Solution {
+	public int dominantIndex(int[] nums) {
+		int largest = 0;
+		int index = 0;
+		int secondLargest = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] > largest) {
+				secondLargest = largest;
+				largest = nums[i];
+				index = i;
+			} else if (nums[i] > secondLargest) {
+				secondLargest = nums[i];
+			}
+		}
+		return largest >= 2 * secondLargest ? index : -1;
+	}
 }
+
+// Runtime: 18 ms
+// Beats: 77.28%
+
+
+
+
+/**
+ * Answer Key
+ * 
+ * 
+
+
+class Solution {
+    public int dominantIndex(int[] nums) {
+        int maxIndex = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] > nums[maxIndex])
+                maxIndex = i;
+        }
+        for (int i = 0; i < nums.length; ++i) {
+            if (maxIndex != i && nums[maxIndex] < 2 * nums[i])
+                return -1;
+        }
+        return maxIndex;
+    }
+}
+
+
+
+
+
+*/
