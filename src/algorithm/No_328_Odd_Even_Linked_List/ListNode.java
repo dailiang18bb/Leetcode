5143e936c3d804dd0cf85f4664cfde66e570f66c
Charles <dailiang18bb@gmail.com>
2019-07-27 14:12:07 -0400

    No 328 code

---
+++ src/algorithm/No_328_Odd_Even_Linked_List/ListNode.java
@@ -0,0 +1,11 @@
+package algorithm.No_328_Odd_Even_Linked_List;
+
+public class ListNode {
+	int val;
+	ListNode next;
+	public ListNode(int x) {
+		val = x;
+		next = null;
+	}
+
+}
---
+++ src/algorithm/No_328_Odd_Even_Linked_List/MainClass.java
@@ -0,0 +1,122 @@
+package algorithm.No_328_Odd_Even_Linked_List;
+
+/* -----------------------------------
+ *  WARNING:
+ * -----------------------------------
+ *  Your code may fail to compile
+ *  because it contains public class
+ *  declarations.
+ *  To fix this, please remove the
+ *  "public" keyword from your class
+ *  declarations.
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+import java.io.*;
+
+class Solution {
+    
+    // Approach 1
+    // Time Complexity: O(n)
+    // Space Complexity: O(1)
+    // Notes: Iteratively
+    public ListNode oddEvenList(ListNode head) {
+        // Edge
+        if(head == null) return null;
+        
+        ListNode oddHead = new ListNode(0);
+        ListNode oddPtr = oddHead;
+        ListNode evenHead = new ListNode(0);
+        ListNode evenPtr = evenHead;
+        ListNode curr = head;
+        int num = 1;
+        
+        while(curr != null){
+            if(num % 2 == 1){
+                oddPtr.next = curr;
+                oddPtr = oddPtr.next;
+            }else{
+                evenPtr.next = curr;
+                evenPtr = evenPtr.next;
+            }
+            curr = curr.next;
+            num++;
+        }
+        evenPtr.next = null;
+        oddPtr.next = evenHead.next;
+        return oddHead.next;
+    }
+    // End of Approach 1
+    
+
+    
+    
+    
+}
+
+public class MainClass {
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+          return new int[0];
+        }
+    
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for(int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+    
+    public static ListNode stringToListNode(String input) {
+        // Generate array from the input
+        int[] nodeValues = stringToIntegerArray(input);
+    
+        // Now convert that list into linked list
+        ListNode dummyRoot = new ListNode(0);
+        ListNode ptr = dummyRoot;
+        for(int item : nodeValues) {
+            ptr.next = new ListNode(item);
+            ptr = ptr.next;
+        }
+        return dummyRoot.next;
+    }
+    
+    public static String listNodeToString(ListNode node) {
+        if (node == null) {
+            return "[]";
+        }
+    
+        String result = "";
+        while (node != null) {
+            result += Integer.toString(node.val) + ", ";
+            node = node.next;
+        }
+        return "[" + result.substring(0, result.length() - 2) + "]";
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            ListNode head = stringToListNode(line);
+            
+            ListNode ret = new Solution().oddEvenList(head);
+            
+            String out = listNodeToString(ret);
+            
+            System.out.print(out);
+        }
+    }
+}
