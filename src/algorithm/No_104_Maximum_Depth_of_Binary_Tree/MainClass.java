604b94609983e59887603964af4d559c76393c38
Charles <dailiang18bb@gmail.com>
2019-09-08 00:10:44 -0400

    No 104 code update

--- src/algorithm/No_104_Maximum_Depth_of_Binary_Tree/MainClass.java
+++ src/algorithm/No_104_Maximum_Depth_of_Binary_Tree/MainClass.java
@@ -1,5 +1,30 @@
 package algorithm.No_104_Maximum_Depth_of_Binary_Tree;
 
+import java.util.*;
+import java.io.*;
+
+/*
+ * 
+ * 
+Given a binary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its depth = 3.
+*/
+
+
 /* -----------------------------------
  *  WARNING:
  * -----------------------------------
@@ -11,121 +36,119 @@ package algorithm.No_104_Maximum_Depth_of_Binary_Tree;
  *  declarations.
  */
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
+//Definition for a binary tree node.
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
 
-import java.util.*;
-import java.io.*;
+	TreeNode(int x) {
+		val = x;
+	}
+}
 
 class Solution {
-    
-    // Approach 1
-    // Time Complexity: O(n)
-    // Space Complexity: O(n)
-    // Notes: Top-Down
-    
+
+	// Approach 1
+	// Time Complexity: O(n)
+	// Space Complexity: O(n)
+	// Notes: Top-Down
+
 //     public int maxDepth(TreeNode root) {
 //         // Edge Case
 //         if(root==null){return 0;}
 //         return maxDepthHelper(root, 1, 1);
 //     }
-    
+
 //     // Helper function
 //     private int maxDepthHelper(TreeNode node, int depth, int maxDep){
 //         if(node == null){return maxDep;}
 //         if(node.left == null && node.right == null){
 //             maxDep = Math.max(depth, maxDep);
 //         }
-        
+
 //         maxDep = maxDepthHelper(node.left, depth + 1, maxDep);
 //         maxDep = maxDepthHelper(node.right, depth + 1, maxDep);   
 //         return maxDep;
 //     }
-    // End of Approach 1
-    
-    
-    
-    // Approach 2
-    // Time Complexity: O(n)
-    // Space Complexity: O(n)
-    // Notes: Bottom-Up
-    
-    public int maxDepth(TreeNode root) {
-        // Edge case
-        if(root == null){return 0;}
-        
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
-        return Math.max(left, right) + 1;
-        
-    }
-    // End of Approach 2
-    
+	// End of Approach 1
+
+	// Approach 2
+	// Time Complexity: O(n)
+	// Space Complexity: O(n)
+	// Notes: Bottom-Up
+
+	public int maxDepth(TreeNode root) {
+		// Edge case
+		if (root == null) {
+			return 0;
+		}
+
+		int left = maxDepth(root.left);
+		int right = maxDepth(root.right);
+		return Math.max(left, right) + 1;
+
+	}
+	// End of Approach 2
+
 }
 
 public class MainClass {
-    public static TreeNode stringToTreeNode(String input) {
-        input = input.trim();
-        input = input.substring(1, input.length() - 1);
-        if (input.length() == 0) {
-            return null;
-        }
-    
-        String[] parts = input.split(",");
-        String item = parts[0];
-        TreeNode root = new TreeNode(Integer.parseInt(item));
-        Queue<TreeNode> nodeQueue = new LinkedList<>();
-        nodeQueue.add(root);
-    
-        int index = 1;
-        while(!nodeQueue.isEmpty()) {
-            TreeNode node = nodeQueue.remove();
-    
-            if (index == parts.length) {
-                break;
-            }
-    
-            item = parts[index++];
-            item = item.trim();
-            if (!item.equals("null")) {
-                int leftNumber = Integer.parseInt(item);
-                node.left = new TreeNode(leftNumber);
-                nodeQueue.add(node.left);
-            }
-    
-            if (index == parts.length) {
-                break;
-            }
-    
-            item = parts[index++];
-            item = item.trim();
-            if (!item.equals("null")) {
-                int rightNumber = Integer.parseInt(item);
-                node.right = new TreeNode(rightNumber);
-                nodeQueue.add(node.right);
-            }
-        }
-        return root;
-    }
-    
-    public static void main(String[] args) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        String line;
-        while ((line = in.readLine()) != null) {
-            TreeNode root = stringToTreeNode(line);
-            
-            int ret = new Solution().maxDepth(root);
-            
-            String out = String.valueOf(ret);
-            
-            System.out.print(out);
-        }
-    }
+	public static TreeNode stringToTreeNode(String input) {
+		input = input.trim();
+		input = input.substring(1, input.length() - 1);
+		if (input.length() == 0) {
+			return null;
+		}
+
+		String[] parts = input.split(",");
+		String item = parts[0];
+		TreeNode root = new TreeNode(Integer.parseInt(item));
+		Queue<TreeNode> nodeQueue = new LinkedList<>();
+		nodeQueue.add(root);
+
+		int index = 1;
+		while (!nodeQueue.isEmpty()) {
+			TreeNode node = nodeQueue.remove();
+
+			if (index == parts.length) {
+				break;
+			}
+
+			item = parts[index++];
+			item = item.trim();
+			if (!item.equals("null")) {
+				int leftNumber = Integer.parseInt(item);
+				node.left = new TreeNode(leftNumber);
+				nodeQueue.add(node.left);
+			}
+
+			if (index == parts.length) {
+				break;
+			}
+
+			item = parts[index++];
+			item = item.trim();
+			if (!item.equals("null")) {
+				int rightNumber = Integer.parseInt(item);
+				node.right = new TreeNode(rightNumber);
+				nodeQueue.add(node.right);
+			}
+		}
+		return root;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String line;
+		while ((line = in.readLine()) != null) {
+			TreeNode root = stringToTreeNode(line);
+
+			int ret = new Solution().maxDepth(root);
+
+			String out = String.valueOf(ret);
+
+			System.out.print(out);
+		}
+	}
 }
