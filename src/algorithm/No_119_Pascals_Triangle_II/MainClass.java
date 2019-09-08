18068ee177c9f3de42d334ce53cc6ecbbc6748e8
Charles <dailiang18bb@gmail.com>
2019-09-07 21:02:01 -0400

    No 119 code update

--- src/algorithm/No_119_Pascals_Triangle_II/MainClass.java
+++ src/algorithm/No_119_Pascals_Triangle_II/MainClass.java
@@ -1,14 +1,34 @@
 package algorithm.No_119_Pascals_Triangle_II;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
+
+/*
+ * 
+ * 
+Given a non-negative index k where k ≤ 33, return the kth index row of the Pascal's triangle.
+
+Note that the row index starts from 0.
+
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it.
+
+Example:
+
+Input: 3
+Output: [1,3,3,1]
+		
+		
+Follow up:
+
+Could you optimize your algorithm to use only O(k) extra space?
+
+*/
+
+import java.awt.Point;
 class Solution {
-    public List<Integer> getRow(int rowIndex) {
+    // public List<Integer> getRow(int rowIndex) {
         
         //Approach 1
         // time out
@@ -57,16 +77,57 @@ class Solution {
         // Time Complexity : O(n^2)
         // Space Complexity: O(n)
         // Notes: Add to the anywhere of a LinkedList cost(O(1)), the use positive search
-        List<Integer> row = new LinkedList<>();
-        for(int i = 0; i < rowIndex + 1; i++){
-            row.add(0,1);
-            for(int k = 1; k < row.size()-1; k++){
-                row.set(k, row.get(k) + row.get(k+1));
-            }
-        }
-        return row;
+        // List<Integer> row = new LinkedList<>();
+        // for(int i = 0; i < rowIndex + 1; i++){
+        //     row.add(0,1);
+        //     for(int k = 1; k < row.size()-1; k++){
+        //         row.set(k, row.get(k) + row.get(k+1));
+        //     }
+        // }
+        // return row;
         // End of Approach 3
+    // }
+    
+    
+    // Approach 4
+    // Notes: Recursion
+    public List<Integer> getRow(int rowIndex) {
+        if(rowIndex < 0){return new ArrayList<>();}
+        ArrayList<Integer> res = new ArrayList<>();
+        HashMap<Point, Integer> hm = new HashMap<>();
+        for(int i = 0; i <= rowIndex; i++){
+            int val = helper(rowIndex, i, hm);
+            res.add(val);
+        }
+        return res;
     }
+    
+    private int helper(int row, int col, HashMap<Point, Integer> hm){
+        if(row == 0 || row == 1){
+            return 1;
+        }
+        if(col == 0 || col == row){
+            return 1;
+        }
+        int one = 0;
+        int two = 0;
+        if(hm.containsKey(new Point(row - 1, col - 1))){
+            one = hm.get(new Point(row - 1, col - 1));
+        }else{
+            one = helper(row - 1, col - 1, hm);
+            hm.put(new Point(row - 1, col - 1), one);
+        }
+        
+        if(hm.containsKey(new Point(row - 1, col))){
+            two = hm.get(new Point(row - 1, col));
+        }else{
+            two = helper(row - 1, col, hm);
+            hm.put(new Point(row - 1, col), two);
+        }
+        return one + two;
+    }
+
+
 }
 
 public class MainClass {
